4504ebb63644434c8d944fb5955d48d7cf560f1f
dritoferro <adrianotagliaferro@hotmail.com>
2019-09-14 11:42:56 -0300

    Added script to start, added fastify and swagger dependencies and removed express.

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import * as endpoint from 'fastify';
+const fast = endpoint({ logger: true })
+
+fast.get('/', async (request, reply) => {
+    return { hello: 'world' }
+})
+
+// Run the server!
+const start = () => {
+    try {
+        fast.listen(3000)
+        fast.log.info(`server listening on ${fast.server.address().port}`)
+    } catch (err) {
+        fast.log.error(err)
+    }
+}
+start();
