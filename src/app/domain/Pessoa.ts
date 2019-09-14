bd712eece8dfb1a39563fbfde8ee465f7b907db3
dritoferro <adrianotagliaferro@hotmail.com>
2019-09-13 21:32:16 -0300

    Removed email from Pessoa.

--- src/app/domain/Pessoa.ts
+++ src/app/domain/Pessoa.ts
@@ -1,13 +1,11 @@
 class Pessoa{
     nome : string;
     documento : string;
-    email : string;
     isPj : boolean = false;
 
-    constructor(nome, email, documento?, isPj?){
+    constructor(nome, documento?, isPj?){
         this.nome = nome;
         this.documento = documento;
-        this.email = email;
         this.isPj = isPj;
     }
 }
