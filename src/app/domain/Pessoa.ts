7e000c3d703ae9207780cd9ffed5aa02fc77f3d2
dritoferro <adrianotagliaferro@hotmail.com>
2019-09-13 21:33:23 -0300

    Code review.

--- src/app/domain/Pessoa.ts
+++ src/app/domain/Pessoa.ts
@@ -1,9 +1,10 @@
 class Pessoa{
+    id : string;
     nome : string;
     documento : string;
     isPj : boolean = false;
 
-    constructor(nome, documento?, isPj?){
+    constructor(nome : string, documento? : string, isPj? : boolean){
         this.nome = nome;
         this.documento = documento;
         this.isPj = isPj;
