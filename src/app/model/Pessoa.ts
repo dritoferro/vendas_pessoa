299bac8d0533d043ab1ea46d22c3c8eed576a9df
Adriano <adrianotagliaferro@hotmail.com>
2020-05-08 21:43:11 -0300

    Applied Prettier format on each file.

--- src/app/model/Pessoa.ts
+++ src/app/model/Pessoa.ts
@@ -1,17 +1,24 @@
 export class Pessoa {
-    _id?: string;
-    nome: string;
-    email: string;
-    isActive: boolean;
-    documento: string;
-    isPj: boolean;
+  _id?: string;
+  nome: string;
+  email: string;
+  isActive: boolean;
+  documento: string;
+  isPj: boolean;
 
-    constructor(nome: string, email: string, id?: string, documento: string = '', isPj: boolean = false, isActive: boolean = true) {
-        this.nome = nome;
-        this.email = email;
-        this._id = id;
-        this.documento = documento;
-        this.isPj = isPj;
-        this.isActive = isActive;
-    }
+  constructor(
+    nome: string,
+    email: string,
+    id?: string,
+    documento: string = '',
+    isPj: boolean = false,
+    isActive: boolean = true
+  ) {
+    this.nome = nome;
+    this.email = email;
+    this._id = id;
+    this.documento = documento;
+    this.isPj = isPj;
+    this.isActive = isActive;
+  }
 }
--- src/app/service/PessoaService.ts
+++ src/app/service/PessoaService.ts
@@ -3,30 +3,37 @@ import { Pessoa } from '../model/Pessoa';
 import { ObjectId } from 'mongodb';
 
 export const insertPessoa = async (pessoa: Pessoa) => {
-    const db = await dbConn();
-    const temp = new Pessoa(pessoa.nome, pessoa.email, pessoa._id, pessoa.documento, pessoa.isPj, pessoa.isActive);
-    const obj = await db.insertOne(temp);
-    return obj.ops;
-}
+  const db = await dbConn();
+  const temp = new Pessoa(
+    pessoa.nome,
+    pessoa.email,
+    pessoa._id,
+    pessoa.documento,
+    pessoa.isPj,
+    pessoa.isActive
+  );
+  const obj = await db.insertOne(temp);
+  return obj.ops;
+};
 
 export const getPessoaById = async (id: string) => {
-    const db = await dbConn();
-    return await db.findOne({ "_id": new ObjectId(id) });
+  const db = await dbConn();
+  return await db.findOne({ _id: new ObjectId(id) });
 };
 
 export const updatePessoaById = async (pessoa: Pessoa, id: string) => {
-    const db = await dbConn();
-    const obj = await db.replaceOne({ "_id": new ObjectId(id) }, pessoa);
+  const db = await dbConn();
+  const obj = await db.replaceOne({ _id: new ObjectId(id) }, pessoa);
 
-    if (obj.modifiedCount) {
-        return true;
-    } else {
-        return false;
-    }
+  if (obj.modifiedCount) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 export const deletePessoaById = async (id: string) => {
-    const db = await dbConn();
-    const obj = await db.deleteOne({ "_id": new ObjectId(id) });
-    return obj.deletedCount;
+  const db = await dbConn();
+  const obj = await db.deleteOne({ _id: new ObjectId(id) });
+  return obj.deletedCount;
 };
